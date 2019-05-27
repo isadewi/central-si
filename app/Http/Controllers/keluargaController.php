a0855562b386141f1a530779c9d59a180d81eca9
sittimutiah <50175835+sittimutiah@users.noreply.github.com>
2019-05-27 15:38:45 +0700

    Edit Fix

--- app/Http/Controllers/keluargaController.php
+++ app/Http/Controllers/keluargaController.php
@@ -1,19 +1,63 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Dosen;
-use App\keluarga;
-
+use App\Keluarga;
 use Illuminate\Http\Request;
 
 class keluargaController extends Controller
 {
+  private $validation_rules = [
+    'nama' => 'required',
+    'hubungan' => 'required',
+    'jenis_kelamin' => 'required',
+    'tempat_lahir' => 'numeric',
+    'tanggal_lahir' => 'numeric',
+    'alamat' => 'required',
+    'no_hp' => 'numeric',
+    'email' => 'required',
+    'pekerjaan' => 'required',
+    'hidup' => 'required'
+  ];
   
     public function index()
     {
         $keluargas = keluarga::paginate(25);
         return view('backend.keluarga.index', compact('keluargas'));
     }
+    
+    public function edit(Keluarga $keluarga)
+    {
+        return view('backend.keluarga.edit', compact('keluarga'));
+    }
+    
+    public function update(Request $request, $keluarga)
+    {
+        $request->validate([
+          'nama',
+          'hubungan',
+          'jenis_kelamin',
+          'tempat_lahir',
+          'tanggal_lahir',
+          'alamat',
+          'no_hp',
+          'email',
+          'pekerjaan',
+          'hidup']);
+        
+        $keluarga = Keluarga::findOrFail($keluarga);
+        $data = $request->all();
+        $keluarga->update($data);
 
-  
+        session()->flash('flash_success', 'Berhasil mengupdate data keluarga '.$keluarga->nama);
+        return redirect()->route('admin.keluarga.index');
+    }
+    
+    public function destroy($id)
+    {
+        $keluarga = Keluarga::find($id);
+        $keluarga->delete($keluarga);
+
+        session()->flash('flash_success', "Berhasil menghapus keluarga ".$keluarga->nama);
+        return redirect()->route('admin.keluarga.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -48,13 +48,15 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/admin/keluarga', 'keluargaController@index')->name('admin.keluarga.index');  //routing lihat daftar mahasiswa
 
 
-     Route::get('/admin/keluarga', 'keluargaController@index')->name('admin.keluarga.index');  //routing lihat daftar mahasiswa
+    Route::get('/admin/keluarga', 'keluargaController@index')->name('admin.keluarga.index');  //routing lihat daftar mahasiswa
     Route::post('/admin/keluarga', 'kekuargaController@store')->name('admin.keluarga.store'); //routing simpan data mahasiswa baru
     Route::get('/admin/keluarga/create', 'keluargaController@create')->name('admin.keluarga.create'); //routing tampilkan form data mahasiswa baru
-    Route::delete('/admin/keluarga/{mahasiswa}', 'keluargaController@destroy')->name('admin.keluarga.destroy'); //routing hapus data mahasiswa baru
-    Route::patch('/admin/keluarga/{mahasiswa}', 'keluargaController@update')->name('admin.keluarga.update'); //routing simpan perubahan data mahasiswa
-    Route::get('/admin/keluarga/{mahasiswa}', 'keluargaontroller@show')->name('admin.keluarga.show'); //routing tampilkan detail mahasiswa
-    Route::get('/admin/keluarga/{mahasiswa}/edit', 'keluargaController@edit')->name('admin.keluarga.edit');  //routing tampilkan form edit mahasiswa
+    Route::delete('/admin/keluarga/{keluarga}', 'keluargaController@destroy')->name('admin.keluarga.destroy'); //routing hapus data mahasiswa baru
+    Route::patch('/admin/keluarga/{keluarga}', 'keluargaController@update')->name('admin.keluarga.update'); //routing simpan perubahan data mahasiswa
+    Route::get('/admin/keluarga/{keluarga}', 'keluargaontroller@show')->name('admin.keluarga.show'); //routing tampilkan detail mahasiswa
+    Route::get('/admin/keluarga/{keluarga}/edit', 'keluargaController@edit')->name('admin.keluarga.edit');  //routing tampilkan form edit mahasiswa
+    Route::get('/admin/keluarga/{keluarga}', 'keluargaController@destroy')->name('admin.keluarga.destroy'); //routing hapus data mahasiswa baru
+    Route::post('/admin/keluarga/{keluarga}', 'keluargaontroller@show')->name('admin.keluarga.show'); //routing tampilkan detail mahasiswa
 
 
 
